9259a260c8b792ce23ed17e748945a5679927c37
Francis Bulu <Francis.Bulu@student.intecbrussel.be>
2020-10-29 15:56:34 +0100

    Add project files.

---
+++ RecursionMeth/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionMeth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef een getal in");
+            int c = Int32.Parse(Console.ReadLine());
+            
+            Console.WriteLine(Factorial(c));
+        }
+        static int Factorial(int a)
+        {
+            if (a==1)
+            {
+                return 1;
+            }
+            return a * Factorial(a - 1);
+
+        }
+    }
+}
